0eeea49b76ffaacf52a93f74c418af7fcc1ae2a0
Ksenya Shestakova <ksenya.shestakova.07@gmail.com>
2022-02-19 16:12:33 +0500

    two classwork

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -9,8 +9,16 @@ def index(title):
     return render_template('base1.html', title=title)
 
 
-"""@app.route('/training/<prof>')
-def prof(profession):"""
+@app.route('/training/<profession>')
+def prof(profession):
+    if 'инженер' in profession.lower() or 'строитель' in profession.lower():
+        return render_template('training.html',
+                               title='InT',
+                               image=url_for('static', filename='img/InT.png'))
+
+    return render_template('training.html',
+                            title='dr',
+                            image=url_for('static', filename='img/dr.png'))
 
 
 if __name__ == '__main__':
