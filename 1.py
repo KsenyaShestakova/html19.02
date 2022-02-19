12a8cffdb3b5d39950745c32ab2f2ebc4c5e66d9
Ksenya Shestakova <ksenya.shestakova.07@gmail.com>
2022-02-19 15:46:38 +0500

    Initial commit

---
+++ 1.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base1.html', title=title)
+
+
+"""@app.route('/training/<prof>')
+def prof(profession):"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
